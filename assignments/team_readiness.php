6ccf2bb68bc3071b103c981d41de418050055199
Ritchie Fitzgerald <ritchiefitz1@gmail.com>
2015-05-04 15:54:38 -0600

    I fixed the team readiness assignment

--- assignments/team_readiness.php
+++ assignments/team_readiness.php
@@ -5,10 +5,10 @@
 </head>
 <body>
 	<?php
-		$name = $nameErr = "";
-		$email = $emailErr = "";
-		$major = $majorErr = "";
-		$places = $placesErr = "";
+		$name = $nameErr = null;
+		$email = $emailErr = null;
+		$major = $majorErr = null;
+		$places = $placesErr = null;
 
 		if ($_SERVER["REQUEST_METHOD"] == "POST") {
 			if (empty($_POST["name"])) {
@@ -62,11 +62,13 @@
 
 		if (isset($name) && isset($email) && isset($major) && isset($places)) {
 			?>
+
 			<p>Name: <?php echo $name; ?></p>
 			<p>Email: <a href="mailto:<?php echo $email; ?>"><?php echo $email; ?></a></p>
 			<p>Major: <?php echo $major; ?></p>
 			<p>Places:<br> <?php echo implode("<br>", $places); ?></p>
 			<p>Comments:<br> <?php echo $comment; ?></p>
+
 			<?php
 		}
 		else {
